1eed112ab98667e1f2ffddd63a349dc26673ba91
rxzhulz <rahulprasetya08@gmail.com>
2025-02-16 10:26:53 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
   
-      const titles = ('SEMANGAT NYET').split('')
+      const titles = ('SEMANGAT').split('')
       const titleElement = document.getElementById('title');
       let index = 0;
   
